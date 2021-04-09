198004d38bda48b11c142cc1f4ef5efe779fadaa
Sean Gilleran <sgilleran@gmail.com>
2021-04-09 16:33:49 -0700

    Build tools version update

--- scripts/shell.bat
+++ scripts/shell.bat
@@ -4,7 +4,7 @@ set scriptsDir=%~dp0
 cd /d %scriptsDir%..
 
 :: set VSCMD_DEBUG=1
-call "C:\Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Applications\Visual Studio\2019\VC\Auxiliary\Build\vcvars64.bat"
 
 set path=%scriptsDir%;%path%;
 set INCLUDE=%scriptsDir%;%INCLUDE%
